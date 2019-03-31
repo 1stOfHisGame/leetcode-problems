5e82f135d74544a50935e759300efece612eb37a
Rushabh Mishra <rushabh.mishra@gmail.com>
2019-03-31 19:23:51 +0530

    Create Solution.java

---
+++ problems/Convert to Base -2/Solution.java
@@ -0,0 +1,17 @@
+
+
+class Solution {
+    public String baseNeg2(int N) {
+        if(N == 0) return "0";
+        String bin = "";
+        int count = 0;
+        while(N != 0){
+            bin = (N % 2) + bin;
+            if(count % 2 == 0)
+                N = (int) Math.floor(N / 2.0);
+            else N = (int) Math.ceil(N / 2.0);
+            count++;
+        }
+        return bin;
+    }
+}
